59f130968a1558ec6d200ae99650d5a86892c69a
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-03-23 11:57:52 +0200

    Создание нового пользователя и запись его в БД

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$(document ).ready(function() {
-    (function loadData() {
-        employeesRef.get().then(data => loadTableData(data));
-    })();
+$(document).ready(function() {
+  loadData();
 
-    function loadTableData(data) {
-        let tableRow = '';
+  function loadData() {
+    employeesRef.get().then(data => loadTableData(data));
+  }
 
-        data.forEach(doc => {
-            const document = doc.data();
-            tableRow += `
+  function loadTableData(data) {
+    let tableRow = '';
+
+    data.forEach(doc => {
+      const document = doc.data();
+      tableRow += `
                 <tr>
                     <td class="fname">${document.fName}</td>
                     <td class="lname">${document.lName}</td>
@@ -16,67 +18,142 @@ $(document ).ready(function() {
                     <td class="age">${document.age}</td>
                     <td class="gender">${document.gender}</td>
                     <td class="yearsofexperience">${
-                        document.yearsOfExprerience
+                      document.yearsOfExperience
                     }</td>
                     <td class="isfulltime">${document.isFullTime}</td>
                     <td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color: green;"></i></td>
                     <td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color: red;"></i></td>
                 </tr>
                 `;
+    });
+    $('tbody.tbodyData').html(tableRow);
+  }
+
+  //get all the data on app startup
+  $('#createEmployee').click(function() {
+    $('.employeeForm').css('display', 'block');
+    $('#dynamicBtn').text('Save Changes');
+  });
+
+  $('#dynamicBtn').click(function() {
+    //employee form values
+    var fname = $('#fname').val();
+    var lname = $('#lname').val();
+    var email = $('#email').val();
+    var age = $('#age').val();
+    var gender = $('#gender').val();
+    var yearsOfExperience = $('#yearsOfExperience').val();
+    var isfulltime = $('#isFullTime').is(':checked');
+
+    //check if you need to create or update an employee
+    if ($(this).text() == 'Save Changes') {
+      // Добавим пользователя, используя его имя как идентификатор документа
+      // Пример: Anna Borisova -> A.Borisova
+      const docId = `${fname[0]}.${lname}`;
+      db
+        .collection('employees')
+        .doc(docId)
+        .set({
+          fName: fname,
+          lName: lname,
+          email,
+          age,
+          gender,
+          yearsOfExperience,
+          isFullTime: isfulltime
+        })
+        .then(docRef => {
+          $('#operationStatus')
+            .html(
+              '<div class="alert alert-success"><strong>Success!</strong> Employee was created!</div>'
+            )
+            .delay(2500)
+            .fadeOut('slow');
+          $('.employeeForm').css('display', 'none');
+          loadData();
+        })
+        .catch(err => {
+          console.log(err);
+          $('#operationStatus')
+            .html(
+              '<div class="alert alert-danger"><strong>Error!</strong> Employee was not created!</div>'
+            )
+            .delay(2500)
+            .fadeOut('slow');
         });
-        $('tbody.tbodyData').html(tableRow);
+    } else {
     }
-  
-    //get all the data on app startup
-    $('#createEmployee').click(function(){
-        $('.employeeForm').css("display", "block");
-        $('#dynamicBtn').text('Save Changes')
-    });
+  });
 
-    $('#dynamicBtn').click(function(){
-        //employee form values
-        var fname = $("#fname").val();
-        var lname = $("#lname").val();
-        var email = $("#email").val();
-        var age = $("#age").val();
-        var gender = $("#gender").val();
-        var yearsOfExperience = $("#yearsOfExperience").val();
-        var isfulltime = $('#isFullTime').is(":checked")
+  // Cancel the Employee form
+  $('#cancel').click(function() {
+    $('.employeeForm').css('display', 'none');
+  });
 
-        //check if you need to create or update an employee
-        if($(this).text() == "Save Changes"){
-        }
-        else{
-        }
-    });
+  // Get the data of the employee you want to edit
+  $('tbody.tbodyData').on('click', 'td.editEmployee', function() {
+    $('.employeeForm').css('display', 'block');
+    $('#dynamicBtn').text('Update Employee');
 
-    // Cancel the Employee form
-    $('#cancel').click(function(){
-        $('.employeeForm').css("display", "none");
-    });
+    $('#fname').val(
+      $(this)
+        .closest('tr')
+        .find('.fname')
+        .text()
+    );
+    $('#lname').val(
+      $(this)
+        .closest('tr')
+        .find('.lname')
+        .text()
+    );
+    $('#email').val(
+      $(this)
+        .closest('tr')
+        .find('.email')
+        .text()
+    );
+    $('#age').val(
+      $(this)
+        .closest('tr')
+        .find('.age')
+        .text()
+    );
+    $('#gender').val(
+      $(this)
+        .closest('tr')
+        .find('.gender')
+        .text()
+    );
+    $('#yearsOfExperience').val(
+      $(this)
+        .closest('tr')
+        .find('.yearsofexperience')
+        .text()
+    );
+    $('#isFullTime').prop(
+      'checked',
+      $(this)
+        .closest('tr')
+        .find('.isfulltime')
+        .text() === 'true'
+    );
+  });
 
-    // Get the data of the employee you want to edit
-    $("tbody.tbodyData").on("click","td.editEmployee", function(){
-        $('.employeeForm').css("display", "block");
-        $('#dynamicBtn').text('Update Employee');
+  // Delete employee
+  $('tbody.tbodyData').on('click', 'td.deleteEmployee', function() {
+    //Get the Employee Data
+    var fName = $(this)
+      .closest('tr')
+      .find('.fname')
+      .text(); //First Name
+    var lName = $(this)
+      .closest('tr')
+      .find('.lname')
+      .text(); //Last Name
+  });
 
-        $("#fname").val($(this).closest('tr').find('.fname').text());
-        $("#lname").val($(this).closest('tr').find('.lname').text());
-        $("#email").val($(this).closest('tr').find('.email').text());
-        $("#age").val($(this).closest('tr').find('.age').text());
-        $("#gender").val($(this).closest('tr').find('.gender').text());
-        $("#yearsOfExperience").val($(this).closest('tr').find('.yearsofexperience').text());
-        $("#isFullTime").prop('checked', $(this).closest('tr').find('.isfulltime').text() === 'true');
-    });
-
-    // Delete employee
-    $("tbody.tbodyData").on("click","td.deleteEmployee", function(){
-        //Get the Employee Data
-        var fName = $(this).closest('tr').find('.fname').text(); //First Name
-        var lName = $(this).closest('tr').find('.lname').text(); //Last Name
-    });
-
-    $("#searchEmployee" ).change(function() {
-        console.log('You entered: ', $(this).val());
-      });
+  $('#searchEmployee').change(function() {
+    console.log('You entered: ', $(this).val());
+  });
 });
